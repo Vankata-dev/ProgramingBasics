d10b44f86cdf7d8c17a997c56651d96a690e02a5
Vankata-dev <79008515+Vankata-dev@users.noreply.github.com>
2021-02-13 15:40:50 +0200

    Add files via upload

---
+++ SumSeconds_1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SumSeconds_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int time1 = Integer.parseInt(scanner.nextLine());
+        int time2 = Integer.parseInt(scanner.nextLine());
+        int time3 = Integer.parseInt(scanner.nextLine());
+        int totalTime = time1 + time2 + time3;
+
+        int sec = totalTime % 60;
+        int min = totalTime / 60;
+
+        if (sec < 10){
+            System.out.printf("%d:0%d", min, sec);
+
+        }else {
+            System.out.printf("%d:%d", min, sec);
+        }
+
+
+
+
+    }
+}
